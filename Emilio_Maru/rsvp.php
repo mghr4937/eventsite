30a1b8f01c4d059545cff96c7b90f5f2aeabce52
marcardini <arcardinimathias@gmail.com>
2016-07-30 17:42:26 -0300

    Completa
    con templates

---
+++ Emilio_Maru/rsvp.php
@@ -0,0 +1,36 @@
+<?php
+
+	// IF VALIDATION ERROR
+	if (isset($_POST["all_error_required"])){
+		$output = json_encode(array('type'=>'error', 'text' => $_POST["all_error_required"][0]));
+		die($output);
+	}
+
+	// IF NO ERROR
+	if (isset($_POST["all_input_id"])){
+
+		$finalmessage = "";
+		foreach ($_POST["all_input_id"] as $input_id) {
+			if (is_array($_POST[$input_id])){
+				$finalmessage .= $_POST[$input_id."_label"]." : ".implode(", ", $_POST[$input_id]). "\n\n";
+			}
+			else
+			{
+				$finalmessage .= $_POST[$input_id."_label"]." : ". $_POST[$input_id] . "\n\n";
+			}
+		}
+
+		$email_to  =  'confirmacion.maruyemi@gmail.com';
+
+		$headers = "From: ".$_POST["inputemail"]."\r\n";
+		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";
+		$subject = $_POST["inputname"]." - CONFIRMA SU ASISTENCIA";
+
+		if(mail($email_to, $subject, $finalmessage, $headers)){
+        	$output = json_encode(array('type'=>'success', 'text' => 'Message Sent'));
+    	}else{
+        	$output = json_encode(array('type'=>'error', 'text' => 'Failed'));
+   		}
+		die($output);
+	}
+?>
